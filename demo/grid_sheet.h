912c4432fd001b0838a317bc307fecc17f22c929
Dmitry Solomennikov <dmitrys99@mail.ru>
2025-07-06 19:42:56 +0300

    Добавлены определения для цветов линий миллиметровой бумаги
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- demo/grid_sheet.h
+++ demo/grid_sheet.h
@@ -6,7 +6,19 @@
 void print_grid(BRST_Doc pdf,
     BRST_Page page, BRST_UINT32 page_lines_color);
 
-// Цвета для линий оранжевой миллимитровой бумаги
+// Цвет линий оранжевой миллимитровой бумаги
 #define BRST_ORANGE_LINE 0xFFB836
+
+// Цвет линий синей миллимитровой бумаги
+#define BRST_BLUE_LINE 0x77DDE7
+
+// Цвет линий зеленой миллимитровой бумаги
+#define BRST_GREEN_LINE 0x00FF7F
+
+// Цвет линий коричневой миллимитровой бумаги
+#define BRST_BROWN_LINE 0x734222
+
+// Цвет линий черной миллимитровой бумаги
+#define BRST_BLACK_LINE 0x181513
 
 #endif /* __GRID_SHEET_H */
