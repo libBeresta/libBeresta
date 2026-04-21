b5bf984f53e02b9bb59cd28afd93bdcc97b2ef4d
Dmitry Solomennikov <dmitrys99@mail.ru>
2026-04-21 10:04:17 +0300

    More CWE-120/CWE-126 fixes
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- demo/cli.c
+++ demo/cli.c
@@ -7,7 +7,7 @@ char _fname[FNAME_BUFFER_SIZE];
 
 char* prepare_output(int argc, char** argv) {
   if (argc > 0 && argv[0] != NULL) {
-      size_t src_len = strlen(argv[0]);
+      size_t src_len = strnlen(argv[0], FNAME_BUFFER_SIZE);
 
       // Проверка, что имя программы не слишком длинное
       if (src_len > FNAME_BUFFER_SIZE - 5) { // -5 для ".pdf" и нулевого байта
@@ -19,7 +19,7 @@ char* prepare_output(int argc, char** argv) {
       _fname[FNAME_BUFFER_SIZE - 1] = '\0';
 
       // Безопасное добавление расширения
-      strncat(_fname, ".pdf", FNAME_BUFFER_SIZE - strlen(_fname) - 1);
+      strncat(_fname, ".pdf", FNAME_BUFFER_SIZE - strnlen(_fname, FNAME_BUFFER_SIZE) - 1);
   } else {
       // Имя по умолчанию, если argv[0] недоступен
       strncpy(_fname, "output.pdf", FNAME_BUFFER_SIZE - 1);
