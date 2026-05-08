29e4af4c18fd88f06e4e63f908f7ebe52345abf1
Dmitry Solomennikov <dmitrys99@mail.ru>
2026-05-08 16:03:51 +0300

    Remove bindings folder
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- bindings/ecl/minimal.lisp
+++
@@ -1,7 +0,0 @@
-(in-package #:cl-beresta)
-
-(with-pdf-document (pdf "minimal.pdf")
-  (let ((page (doc-page-add pdf)))
-    (page-setsize page
-                  page-size-a4
-                  page-orientation-landscape)))
