383d08e96db7f61e3b149838722a5ca1c7eee46a
Dmitry Solomennikov <dmitrys99@mail.ru>
2026-08-06 10:35:25 +0300

    Remove private header from brst.h
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- include/private/brst_utils.h
+++ include/private/brst_utils.h
@@ -144,26 +144,5 @@ BRST_PageSizeName(
                                   c == 0x0D || \
                                   c == 0x20 )  \
 
-/*----------------------------------------------------------------------------*/
-/*----- macros for debug -----------------------------------------------------*/
-
-#ifdef LIBBRST_DEBUG_TRACE
-#ifndef BRST_PTRACE_ON
-#define BRST_PTRACE_ON
-#endif /* BRST_PTRACE_ON */
-#endif /* LIBBRST_DEBUG_TRACE */
-
-#ifdef BRST_PTRACE_ON
-#define BRST_PTRACE(...)  BRST_PRINTF(__VA_ARGS__)
-#else
-#define BRST_PTRACE(...)  /* do nothing */
-#endif /* BRST_PTRACE */
-
-#ifdef LIBBRST_DEBUG
-#define BRST_PRINT_BINARY(BUF, LEN, CAPTION) BRST_PrintBinary(BUF, LEN, CAPTION)
-#else
-#define BRST_PRINT_BINARY(BUF, LEN, CAPTION) /* do nothing */
-#endif
-
 #endif /* PRIVATE_BRST_UTILS_H */
 
