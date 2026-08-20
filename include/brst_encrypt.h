f9653f25800a6b845d41fb81231e04a9cfe263ac
Dmitry Solomennikov <dmitrys99@mail.ru>
2026-08-20 14:00:05 +0300

    Expand EncryptMode enum

--- include/brst_encrypt.h
+++ include/brst_encrypt.h
@@ -2,8 +2,11 @@
 #define BRST_ENCRYPT_H
 
 typedef enum _BRST_EncryptMode {
-    BRST_ENCRYPT_R2    = 2,
-    BRST_ENCRYPT_R3    = 3
+    BRST_ENCRYPT_R0 = 0,
+    BRST_ENCRYPT_R1 = 1,
+    BRST_ENCRYPT_R2 = 2,
+    BRST_ENCRYPT_R3 = 3,
+    BRST_ENCRYPT_R4 = 4
 } BRST_EncryptMode;
 
 #endif /* BRST_ENCRYPT_H */
